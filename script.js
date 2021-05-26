fe8f4d7ac64447dc557939b168874e371e54a16a
Mark Manipula <markmikomanipula@gmail.com>
2021-05-26 10:51:27 -0700

    cleaned script.js

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 document.getElementById("myBtn").addEventListener("click", submitPhotos);
-
-function reloadPictures() {
-    e.preventDefault();
-
-    fetch(`${baseUrl}/display20pictures`)
-        .then((response) => response.json())
-        .then((pictures) => { });
-}
-
 const baseUrl = `https://project-free99.herokuapp.com`;
 
-//this will display 20 pictures based on the user input
+//this will add data to our database to be loaded later
 async function submitPhotos(e) {
     e.preventDefault();
 
@@ -30,7 +21,7 @@ async function submitPhotos(e) {
 
 document.getElementById("done").addEventListener("click", displayPictures);
 
-//loop through the database and display a picture
+//this will load all the photos submitted by the user
 function displayPictures(e) {
     e.preventDefault();
 
@@ -63,63 +54,8 @@ function displayPictures(e) {
 
 
 //make a function that keep traks of the like and dislikes. all the like photos should be added to the firstReviewLikes collection
-function likeDislikeTracker() {
+function addOrRemove() {
 
-}
-
-document.getElementById("Like").addEventListener("click", zeroToFivePics);
-//make a function that puts 0-5 photos in the third page, and should be added to the secondReviewLikes collection
-function zeroToFivePics() {
-    document.create
-}
-
-//this is where we call initial POST from our backend
-async function addInitialCards(e) {
-    //keep it from refreshing
-    e.preventDefault();
-
-    const userDestinationInput = document.getElementById("destination").value;
-    const userLocationInput = document.getElementById("location").value;
-
-    const response = await fetch(`${baseUrl}/firstReview`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            destination: userDestinationInput,
-            location: userLocationInput,
-        }),
-    });
-    await response.json();
-    location.reload();
-}
-
-function handleSubmit(e) {
-    e.preventDefault();
-
-    const userDestinationInput = document.getElementById("destination").value;
-    const userLocationInput = document.getElementById("location").value;
-
-    //fetches backend api
-    fetch(url)
-        .then((response) => response.json())
-        .then((pictures) => addPictures(pictures.results));
-}
-
-function addPictures(pictures) {
-    const random = Math.floor(Math.random() * pictures.length);
-    const photoURL = pictures[random].urls.thumb;
-
-    const userDestinationInput = document.getElementById("destination").value;
-    const userLocationInput = document.getElementById("location").value;
-
-    document.createElement("div").classList.add("card");
-
-    document.createElement("div").innerHTML = `
-    <div class="card-body">
-    <img class="card-img-top" src =${photoURL}>
-        <h5 class="card-title">${userDestinationInput}</h5>
-        <p class="card-text">${userLocationInput}</p>
-    </div>`;
 }
 
 function resetForm() {
@@ -132,25 +68,3 @@ function deleteCard(btn) {
     btn.parentElement.parentElement.remove();
 }
 
-function handleEdit(e) {
-    const oldDestination = e.parentElement.children[0];
-    const oldLocation = e.parentElement.children[1];
-
-    const oldPhoto = e.parentElement.parentElement.children[0];
-
-    const newDestination = prompt("New Destination", oldDestination.innerText);
-    const newLocation = prompt("New Location", oldLocation.innerText);
-    const newPhoto = promt("New Photo", oldPhoto.getAttribute("src"));
-
-    if (newDestination !== "") {
-        oldLocation.innerText = newDestination;
-    }
-
-    if (newLocation !== "") {
-        oldLocation.innerText = newLocation;
-    }
-
-    if (newPhoto !== "") {
-        oldPhoto.setAttribute("src", newPhoto);
-    }
-}
